da9504447a1ca5be7b3ae00e3d78b36438923fb3
Varad Mule <varadmule17@gmail.com>
2025-09-21 21:58:15 +0530

    deleted

--- tms-backend/src/main/java/com/tms/tms_backend/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tms.tms_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tms.tms_backend.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
--- tms-backend/src/main/java/com/tms/tms_backend/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.tms.tms_backend.service;
-
-import com.tms.tms_backend.dto.LoginDto;
-import com.tms.tms_backend.dto.RegisterDto;
-
-public interface AuthService {
-	String login(LoginDto loginDto);
-	String register(RegisterDto registerDto);
-}
